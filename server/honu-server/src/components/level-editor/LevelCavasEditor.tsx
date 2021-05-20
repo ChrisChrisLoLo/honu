f771f3da55dcf94a370c3cbf42f840e3330f1c02
ChrisChrisLoLo <sporewoh@gmail.com>
2021-05-20 17:11:50 -0600

    update the editor

--- server/honu-server/src/components/level-editor/LevelCavasEditor.tsx
+++ server/honu-server/src/components/level-editor/LevelCavasEditor.tsx
@@ -45,16 +45,35 @@ export default function LevelCanvasEditor(props: PropType) {
     props.setTestCase(newTestCase)
   }
 
+  function handleCoordChange(valString: string, isX: boolean) {
+    const newCoord = parseInt(valString)
+    const newTestCase = { ...props.testCase }
+
+    isX ? newTestCase.levelData.player.pos.x = newCoord : newTestCase.levelData.player.pos.y = newCoord
+    props.setTestCase(newTestCase)
+  }
+
+  function handleDirChange(e: any) {
+    const dirString: DirectionType = e.target.value
+    const newTestCase = { ...props.testCase }
+    newTestCase.levelData.player.dir = dirString
+    props.setTestCase(newTestCase)
+
+  }
+
   function handleTileToDraw(e: any) {
     setTileToDraw(e.target.value)
   }
+
+  const width = props.testCase.levelData.level[0].length
+  const height = props.testCase.levelData.level.length
 
   return (
     <>
       <HStack>
         <FormControl>
           <FormLabel>Level Width</FormLabel>
-          <NumberInput min={1} value={props.testCase.levelData.level[0].length} name="width" onChange={(valueString) => handleDimChange(valueString, true)}>
+          <NumberInput min={1} value={width} name="width" onChange={(valueString) => handleDimChange(valueString, true)}>
             <NumberInputField />
             <NumberInputStepper>
               <NumberIncrementStepper />
@@ -64,7 +83,29 @@ export default function LevelCanvasEditor(props: PropType) {
         </FormControl>
         <FormControl>
           <FormLabel>Level Height</FormLabel>
-          <NumberInput min={1} value={props.testCase.levelData.level.length} name="height" onChange={(valueString) => handleDimChange(valueString, false)}>
+          <NumberInput min={1} value={height} name="height" onChange={(valueString) => handleDimChange(valueString, false)}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
+      </HStack>
+      <HStack>
+        <FormControl>
+          <FormLabel>Turtle X</FormLabel>
+          <NumberInput min={0} max={width-1} onChange={(valueString) => handleCoordChange(valueString, true)}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Turtle Y</FormLabel>
+          <NumberInput min={0} max={height-1} onChange={(valueString) => handleCoordChange(valueString, false)}>
             <NumberInputField />
             <NumberInputStepper>
               <NumberIncrementStepper />
@@ -84,7 +125,6 @@ export default function LevelCanvasEditor(props: PropType) {
         </Select>
         <FormHelperText>Click on the tiles to color them</FormHelperText>
       </FormControl>
-
       <GameCanvas testCase={props.testCase} setTestCase={props.setTestCase} selectedTileType={tileToDraw} isExpectedOutput={props.isExpectedOutput}></GameCanvas>
     </>
   )
