4bae69d3cd0095b8842bfcc0808a55726609d383
ChrisChrisLoLo <sporewoh@gmail.com>
2021-05-30 17:38:46 -0600

    Add copy JSON button for level export

--- server/honu-server/src/components/level-editor/LevelImportExport.tsx
+++ server/honu-server/src/components/level-editor/LevelImportExport.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Stack, Text } from '@chakra-ui/react';
+import { Button, HStack, Input, Stack, Text, useToast } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { MetaGame } from '../../types/MetaGame';
 import { TestCase } from '../../types/TestCase';
@@ -12,27 +12,45 @@ interface PropType {
 
 export default function LevelImportExport(props: PropType) {
 
-  function downloadMetagameAsJson(exportMetagame: MetaGame) {
+  const toast = useToast()
 
-    // Deep copy the metagame as modifications are made to it
-    const exportMetagameCopy: MetaGame = JSON.parse(JSON.stringify(exportMetagame));
+  function metagameToJson(metagame: MetaGame): MetaGame {
+    const metagameCopy: MetaGame = JSON.parse(JSON.stringify(metagame));
 
     // Remove the expected board type if it exists when the win condition is not to modify the board
-    if(exportMetagameCopy.winCondition !== WinCondType.MODIFY_BOARD){
-      for( let testCase of exportMetagameCopy.testCases){
+    if (metagameCopy.winCondition !== WinCondType.MODIFY_BOARD) {
+      for (let testCase of metagameCopy.testCases) {
         delete testCase.expectedLevel
       }
     }
+    return metagameCopy
+  }
+
+  function downloadMetagameAsJson(exportMetagame: MetaGame): void {
+    const exportMetagameCopy = metagameToJson(exportMetagame)
 
     const exportName: string = `${exportMetagameCopy.levelId}-${exportMetagameCopy.title.toLowerCase().trim().replaceAll(' ', '_')}`
     const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportMetagameCopy, undefined, 2));
-    const downloadAnchorNode = document.createElement('a');
-    downloadAnchorNode.setAttribute("href", dataStr);
-    downloadAnchorNode.setAttribute("download", exportName + ".json");
-    document.body.appendChild(downloadAnchorNode); // required for firefox
-    downloadAnchorNode.click();
-    downloadAnchorNode.remove();
+    const downloadAnchorNode = document.createElement('a')
+    downloadAnchorNode.setAttribute("href", dataStr)
+    downloadAnchorNode.setAttribute("download", exportName + ".json")
+    document.body.appendChild(downloadAnchorNode) // required for firefox
+    downloadAnchorNode.click()
+    downloadAnchorNode.remove()
   }
+
+  function copyJsonToClipboard(exportMetagame: MetaGame): void {
+    const exportMetagameCopy = metagameToJson(exportMetagame)
+    navigator.clipboard.writeText(JSON.stringify(exportMetagameCopy))
+
+    toast({
+      title: "JSON Copied!",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+    })
+  }
+
 
   /**
    * Read a metagame JSON level
@@ -45,7 +63,7 @@ export default function LevelImportExport(props: PropType) {
     const file: File = filelist[0]
     const text = await file.text()
     const metagame: MetaGame = JSON.parse(text)
-    metagame.testCases = metagame.testCases.map((testCase)=>populateExpectedLevelIfNotExists(testCase))
+    metagame.testCases = metagame.testCases.map((testCase) => populateExpectedLevelIfNotExists(testCase))
     props.setMetagame(metagame)
   }
 
@@ -55,7 +73,7 @@ export default function LevelImportExport(props: PropType) {
    */
   function populateExpectedLevelIfNotExists(testCase: TestCase): TestCase {
     const newTestCase: TestCase = { ...testCase }
-    if(!newTestCase.expectedLevel){
+    if (!newTestCase.expectedLevel) {
       newTestCase.expectedLevel = JSON.parse(JSON.stringify(newTestCase.levelData.level));
     }
     return newTestCase
@@ -64,9 +82,14 @@ export default function LevelImportExport(props: PropType) {
   return (
     <Stack>
       <Text>Export</Text>
-      <Button onClick={() => downloadMetagameAsJson(props.metagame)}>
-        Download
-      </Button>
+      <HStack>
+        <Button onClick={() => downloadMetagameAsJson(props.metagame)}>
+          Download
+        </Button>
+        <Button onClick={() => copyJsonToClipboard(props.metagame)}>
+          Copy JSON
+        </Button>
+      </HStack>
       <Text>Import</Text>
       <Input type="file" onChange={handleMetagameFileInput} ></Input>
     </Stack>
